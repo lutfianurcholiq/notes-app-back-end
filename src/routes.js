389d40c00df1d31d0b8351c882f185654209833c
lutfianurcholiq <lutfian.nurcholiq@gmail.com>
2023-11-11 08:52:13 +0700

    first upload project node js"

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNodeHandler } = require('./handler')
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNodeHandler
+  }
+]
+
+module.exports = routes
