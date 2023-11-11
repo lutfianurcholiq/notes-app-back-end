145ae50c4819cd59849792f1004a85a7f0f31675
lutfianurcholiq <lutfian.nurcholiq@gmail.com>
2023-11-11 11:40:58 +0700

    update file vercel.json

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const routes = require('./routes')
 
 const init = async () => {
     const server = Hapi.server({
-        port: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        port: 5000,
         host: 'localhost',
         routes: {
             cors: {
